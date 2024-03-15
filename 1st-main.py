bb7691e5d49f1d418321de56cb39791801afdab2
Yoo JaeMyeong <45450868+yoojaemyeong@users.noreply.github.com>
2024-03-16 07:31:34 +0900

    Update 1st-main.py

--- 1st-main.py
+++ 1st-main.py
@@ -1,14 +1,17 @@
 import sys
- from PyQt5.QtWidgets import QApplication, QWidget
- class Calculator(QWidget):
-     def __init__(self):
-         super().__init__()
-         self.initUI()
-     def initUI(self):
-         self.setWindowTitle(‘계산기’)
-         self.resize(256,256)
-         self.show()
- if __name__=='__main__':
-     app = QApplication(sys.argv)
-     view = Calculator()
-     sys.exit(app.exec_())
+from PyQt5.QtWidgets import QApplication, QWidget
+class Calculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Calculator')
+        self.resize(256,256)
+        self.show()
+
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    view = Calculator()
+    sys.exit(app.exec_())
